cfac822d788dab7b46970ef7840082f3df1f11d6
HassanAlee <hassanali14563@gmail.com>
2025-02-21 22:53:26 +0500

    learned filtering and geouping tests

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,13 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-test("greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/Hello/i);
-  expect(textElement).toBeInTheDocument();
-});
-test("greet renders with a name", () => {
-  render(<Greet name="john" />);
-  const textElement = screen.getByText(/Hello john/);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/Hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+  test("renders with a name", () => {
+    render(<Greet name="john" />);
+    const textElement = screen.getByText(/Hello john/);
+    expect(textElement).toBeInTheDocument();
+  });
 });
